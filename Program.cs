8174ebd97a461d3f8b3e8603ba5a062fb9e6f7f6
Srijani Ghosh <srijanighosh87@gmail.com>
2025-09-04 17:01:52 +0200

    Initial commit of the project. 3 tools has been added.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer() // adds MCP server support
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly() // automatically registers all tools in the current assembly
+    ;
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    // This method is automatically registered as an MCP tool 
+    // because of the McpServerToolType attribute on the class
+    // and the McpServerTool attribute on the method.
+
+    /// <summary>
+    /// Echoes the message back to the client.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C# : {message}";
+
+    /// <summary>
+    /// Reverses the message.
+    /// </summary>
+    /// <param name="message">The message to be reversed.</param>
+    [McpServerTool, Description("Reverses the message.")]
+    public static string Reverse(string message) => $"Reverse message : {message.Reverse().ToString()}";
+
+    /// <summary>
+    /// Returns the length of the message.
+    /// </summary>
+    /// <param name="message">The message for which the length has to be calculated.</param>
+    [McpServerTool, Description("Calculate the length of the message.")]
+    public static string Length(string message) => $"Message length : {message.Length}";
+}
